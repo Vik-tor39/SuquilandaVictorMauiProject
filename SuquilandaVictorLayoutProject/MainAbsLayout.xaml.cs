ddd525a4db7eeff094f125653a572c99b05e8373
Victor Suquilanda <victor.suquilanda@udla.edu.ec>
2024-11-20 00:16:46 -0500

    Commit 7: Se intentó añadir un Grid Layout en la última vista (Absolute Layout) para almacenar un scrooll. Aún faltan detalles por afinar.

--- SuquilandaVictorLayoutProject/MainAbsLayout.xaml.cs
+++ SuquilandaVictorLayoutProject/MainAbsLayout.xaml.cs
@@ -6,6 +6,10 @@ public partial class MainAbsLayout : ContentPage
 	{
 		InitializeComponent();
 	}
+    void OnScrollViewScrolled(object sender, ScrolledEventArgs e)
+    {
+        Console.WriteLine($"ScrollX: {e.ScrollX}, ScrollY: {e.ScrollY}");
+    }
     private void NuevaPagina_Clicked(object sender, EventArgs e)
     {
         Navigation.PushAsync(new MainGrid());
